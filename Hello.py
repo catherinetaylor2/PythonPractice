645064a0bad127bee360bc27b8c521a41e8f6cd2
Catherine <cat.taylor.taylor@btinternet.com>
2017-09-14 19:31:50 +0100

    Added if loops

--- Hello.py
+++ Hello.py
@@ -1,1 +1,17 @@
 print("hello world");
+
+x = 3;
+
+if x == 4:
+    print("x is", 4);
+else:
+    print("x is not", 4);
+
+f = 7.0;
+g = float(7);
+
+if f==g:
+    print("match");
+
+string = "HELLO";
+print(string);
