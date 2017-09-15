ee8160f71dfd34f39603a8dbf04426a4e706e16f
Catherine <cat.taylor.taylor@btinternet.com>
2017-09-15 10:18:20 +0100

    Commented code

--- Hello.py
+++ Hello.py
@@ -2,6 +2,7 @@ import numpy as np
 
 print("hello world");
 
+#IF LOOP
 x = 3;
 if x == 4:
     print("x is", 4);
@@ -17,11 +18,13 @@ if f==g:
 string = "HELLO";
 print(string);
 
+#FOR LOOPS
 for i in range(0,5):
-    print("x = ", i);
+   print("x = ", i);
 
-a = np.array([1,2,3]);
+a = np.array([1,2,3]); #numpy array
 print("a = ", a);
+print("a type :", a.dtype);
 
 for i in a:
     print(i**3);
@@ -30,10 +33,9 @@ for i in a:
 def square(x):
     return x**2;
 
+#MAP FUNCTION
 for i in (map(square, a)):
-    print ("i = ", i);
-
-
+   print ("i = ", i);
 
 
 b = [square(x) for x in a];
@@ -42,6 +44,7 @@ print("b = ", b);
 c = [*map(square,a)];
 print("c = ", c);
 
+#CONVERT TO ARRAY
 B = np.array(b);
 print("B =", B);
 print(B.shape);
