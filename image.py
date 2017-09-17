242e542403a7aa16a2d2e428b3b7191991c26df3
Catherine <cat.taylor.taylor@btinternet.com>
2017-09-17 11:45:20 +0100

    image

--- image.py
+++ image.py
@@ -2,4 +2,12 @@ from PIL import Image
 
 
 img = Image.open('cat.jpg');
-img.show();
+#img.show();
+
+r, g, b = img.split()
+im = Image.merge("RGB", (b, g, r))
+
+rgbIm = img.convert('RGB');
+R,G,B = rgbIm.getpixel((0,0));
+
+print(R,G,B);
