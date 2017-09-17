f37d9b3a112b22c768a005f69a59fb136b71bfc0
Catherine <cat.taylor.taylor@btinternet.com>
2017-09-17 15:58:21 +0100

    Coded convolution python

--- image.py
+++ image.py
@@ -1,4 +1,5 @@
 from PIL import Image
+import numpy as np 
 
 
 img = Image.open('cat.jpg');
@@ -10,4 +11,15 @@ im = Image.merge("RGB", (b, g, r))
 rgbIm = img.convert('RGB');
 R,G,B = rgbIm.getpixel((0,0));
 
-print(R,G,B);
+ #print(R,G,B);
+
+I = Image.new(img.mode, (500,500));
+#rI, gI, bI = I.split();
+
+
+for i in range(0,500):
+    for j in range(0,500):
+        I.putpixel((i,j),(255,0,0));
+
+#I = Image.merge("RGB", (rI, gI, bI));
+I.show();
