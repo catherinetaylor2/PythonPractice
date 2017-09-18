3aa399525380240d49cea274c533e775b6d57739
Catherine <cat.taylor.taylor@btinternet.com>
2017-09-18 13:46:23 +0100

    More work with convolution

--- convolution.py
+++ convolution.py
@@ -2,24 +2,23 @@ from PIL import Image
 import numpy as np
 
 img = Image.open('cat.jpg');
+I = Image.new("RGB", img.size);
 
-#kernel = 1/16*np.array([[1,2,1], [2,4,2], [1,2,1]]);
-kernel = np.array([[ 1, 0, -1], [0, 0, 0], [-1, 0, 1]]);
+#kernel = 1/16*np.array([[1,2,1], [2,4,2], [1,2,1]]); #gaussian blur
+kernel = np.array([[ 1, 0, -1], [0, 0, 0], [-1, 0, 1]]); #edge detection
+
 k = kernel.shape[1];
 N = (k-1)//2;
-I = Image.new(img.mode, img.size);
 
-A = np.array(img);
-E = np.pad(A, (N,N), 'reflect');
+paddedImage = np.pad(np.array(img), (N,N), 'reflect');
 
-print(E.shape)
 for y in range(0,img.size[0]):
     for x in range(0, img.size[1]):
         sum = np.array([0,0,0]);
         for i in range(0,k):
             for j in range(0,k):
-                      sum = sum + E[x+2*N-i, y+2*N-j][1:4]*kernel[i,j];
-        I.putpixel((y,x), (np.int(sum[0]),np.int(sum[1]),np.int(sum[2])));
+                      sum = sum + paddedImage[x+2*N-i, y+2*N-j][1:4]*kernel[i,j];
+        I.putpixel((y,x), (np.int(sum[0]),np.int(sum[1]),np.int(sum[2]))); #pillow doesn't support floating point RGB
 
 
 I.show();
