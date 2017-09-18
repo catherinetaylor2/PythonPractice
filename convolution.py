0aebe58d6cbfd2404cf325bd98e05f4adc36e1cf
Catherine <cat.taylor.taylor@btinternet.com>
2017-09-18 14:00:15 +0100

    Added subplots

--- convolution.py
+++ convolution.py
@@ -1,11 +1,13 @@
 from PIL import Image
 import numpy as np
+import matplotlib.pyplot as plt
 
 img = Image.open('cat.jpg');
 I = Image.new("RGB", img.size);
 
-#kernel = 1/16*np.array([[1,2,1], [2,4,2], [1,2,1]]); #gaussian blur
-kernel = np.array([[ 1, 0, -1], [0, 0, 0], [-1, 0, 1]]); #edge detection
+
+kernel = 1/16*np.array([[1,2,1], [2,4,2], [1,2,1]]); #gaussian blur
+#kernel = np.array([[ 1, 0, -1], [0, 0, 0], [-1, 0, 1]]); #edge detection
 
 k = kernel.shape[1];
 N = (k-1)//2;
@@ -21,7 +23,20 @@ for y in range(0,img.size[0]):
         I.putpixel((y,x), (np.int(sum[0]),np.int(sum[1]),np.int(sum[2]))); #pillow doesn't support floating point RGB
 
 
-I.show();
+#Display before and after images
+fig = plt.figure();
+a=fig.add_subplot(1,2,1)
+imgPlot = plt.imshow(img);
+a.set_title('Original image')
+a.set_yticklabels([])
+a.set_xticklabels([])
+a=fig.add_subplot(1,2,2)
+imgPlot = plt.imshow(I);
+a.set_title('Filtered image')
+a.set_yticklabels([])
+a.set_xticklabels([])
+
+plt.show();
 
 
 
