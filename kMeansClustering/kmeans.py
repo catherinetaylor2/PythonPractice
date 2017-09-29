4a6265ddee562f232b3c8b8c1cc1b6c469154009
Catherine <cat.taylor.taylor@btinternet.com>
2017-09-29 18:08:32 +0100

    Chosen initial mean for each set

--- kMeansClustering/kmeans.py
+++ kMeansClustering/kmeans.py
@@ -1,10 +1,23 @@
 import numpy as np 
 import geyserData as GD
 import matplotlib.pyplot as pl 
+import random
 
-data = GD.getData(0);
+data = (GD.getData(0));
 
-pl.plot(data[:,0], data[:,1], 'o');
-pl.show(); 
+
+#pl.plot(data[:,0], data[:,1], 'o');
+#pl.show(); 
+
+#chose initial point in each cluster
+
+p1 = random.randint(0, data.shape[0]);
+p2 =p1;
+while (p2 == p1):
+ p2 = random.randint(0, data.shape[0]);
+initialPoint1 = data[p1,:];
+initialPoint2 = data[p2,:];
+
+print(p1, p2)
 
 
